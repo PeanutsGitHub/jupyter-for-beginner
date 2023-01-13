80abcd9b9837dcce316f2118a8436f50175b1942
PeanutsGitHub <117813804+PeanutsGitHub@users.noreply.github.com>
2023-01-13 21:34:34 +0700

    Add files via upload

---
+++ Python-Matplotlib/06-Histograms/finished_code.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+import os
+# Change the current working directory
+os.chdir(os.path.dirname(__file__))
+
+plt.style.use('fivethirtyeight')
+
+data = pd.read_csv('data.csv')
+ids = data['Responder_id']
+ages = data['Age']
+
+bins = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+plt.hist(ages, bins=bins, edgecolor='black', log=True)
+
+median_age = 29
+color = '#fc4f30'
+
+plt.axvline(median_age, color=color, label='Age Median', linewidth=2)
+
+plt.legend()
+
+plt.title('Ages of Respondents')
+plt.xlabel('Ages')
+plt.ylabel('Total Respondents')
+
+plt.tight_layout()
+
+plt.show()
---
+++ Python-Matplotlib/07-ScatterPlots/finished_code.py
@@ -0,0 +1,31 @@
+
+import pandas as pd
+from matplotlib import pyplot as plt
+
+import os
+# Change the current working directory
+os.chdir(os.path.dirname(__file__))
+
+plt.style.use('seaborn')
+
+data = pd.read_csv('2019-05-31-data.csv')
+view_count = data['view_count']
+likes = data['likes']
+ratio = data['ratio']
+
+plt.scatter(view_count, likes, c=ratio, cmap='summer',
+            edgecolor='black', linewidth=1, alpha=0.75)
+
+cbar = plt.colorbar()
+cbar.set_label('Like/Dislike Ratio')
+
+plt.xscale('log')
+plt.yscale('log')
+
+plt.title('Trending YouTube Videos')
+plt.xlabel('View Count')
+plt.ylabel('Total Likes')
+
+plt.tight_layout()
+
+plt.show()
